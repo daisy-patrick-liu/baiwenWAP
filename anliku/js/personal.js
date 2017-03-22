a8ab08eb80750086841d3bd5802b130f265fa734
daisy-liu <954654261@qq.com>
2017-03-22 10:07:48 +0800

    test

--- anliku/js/personal.js
+++ anliku/js/personal.js
@@ -1,6 +1,6 @@
 $(function(){
     $.extend({
-        top:function(){//返回顶部
+        top:function(){//返回顶部--liu add
             $("body,html").animate({scrollTop:0},300);
         },
         showCharts:{//封装echarts弹框于此对象
